b3db059d4d07d1046b36025cfaf54a9547ea876a
glorianachen <57929370+glorianachen@users.noreply.github.com>
2020-02-02 22:39:26 -0500

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 21:36:26 2020
+
+@author: chenjiayu
+"""
+
+import time
+from scipy.stats import poisson
+import random
+import numpy as np
+import threading
+
+
+def intersection(bus):
+    # r is the poisson generated number of cars when a bus gets in intersection, 10 estimated as mean
+    r = poisson.rvs(3, size=1);r=r[0]
+    def timerprint():
+        print('bus starts and goes through intersection at'+str(time.time()))
+    print('bus enter intersection')
+    if r>=5:
+        """
+        here time is absolute, need replace with the one in main. Use the relative time .
+        """
+        print('bus stops at intersection at'+str(time.time()))
+        timer=threading.Timer(2*r,timerprint)
+        timer.start()
+    else:
+        print('bus goes through intersection'+str(time.time()))
+    
